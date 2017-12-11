439ff3c174697e6a0ac1cadfdb82aecc6c7e2129
Duane Reymond <duane.reymond@gmail.com>
2017-12-11 20:03:56 +1100

    -First Commit: FlappyBird.

---
+++ Assets/~FlappyBird/Scripts/Columns/Column.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBird
+{
+    public class Column : MonoBehaviour
+    {
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            // Have we collided with the bird?
+            if (other.name.StartsWith("Bird")) // or .Contains("Bird")
+            {
+                // Then the bird scored
+                GameManager.Instance.BirdScored();
+            }
+        }
+    }
+}
---
+++ Assets/~FlappyBird/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBird
+{
+    public class GameManager : MonoBehaviour
+    {
+        public bool gameOver = false;
+        public float scrollSpeed = -1.5f;
+        public int score = 0;
+
+        public static GameManager Instance = null;
+
+        public delegate void ScoreAddedCallback(int score);
+        public ScoreAddedCallback scoreAdded;
+
+        // Use this for initialization
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        public void BirdScored()
+        {
+            // The bird cant score if there is a game over
+            if(gameOver)
+            {
+                // Exit the function
+                return;
+            }
+
+            // Increase the score
+            score++;
+
+            // If there is a function subscribed
+            if (scoreAdded != null)
+            {
+                // Call an event to state that a score has been added
+                scoreAdded.Invoke(score);
+            }
+        }
+
+        public void BirdDied()
+        {
+            // Set game over to true
+            gameOver = true;
+        }
+    }
+}
